a7a85e68914f838db7e0a82e64b3e0619dbb018d
Dedalino <kelsonpapelo18@gmail.com>
2023-09-15 00:20:04 +0100

    setting up vercel configurations for deployment

--- server.js
+++ server.js
@@ -53,8 +53,9 @@ app.use(function(err, req, res, next) {
   }  
 })
 
+module.exports = app
+
 //Listen on port set in environment variable or default to 3000
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
-
